be77f631f36990b0a1d62f6988936c7ef00d6d87
Dilani040 <dilanichandrakumar@gmail.com>
2023-01-02 16:11:06 +0530

    multi file added

---
+++ Sub.java
@@ -0,0 +1,12 @@
+class Sub
+{
+	public static void main(String args [])
+	{
+		int a=3;
+		int b=5;
+		int c=10;
+		int d=9;
+		System.out.println(a-b);
+        //Important file
+	}
+}
--- div.java
+++ div.java
@@ -1,4 +1,4 @@
-class Sum
+class Div
 {
 	public static void main(String args [])
 	{
